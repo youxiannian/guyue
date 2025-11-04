ff165993875a6629e8a46545ce9c4aa8332f9644
youxiannian <2091478243@qq.com>
2025-11-04 18:42:51 +0800

    删除目录：NO_15_test_3_29

--- NO_15_test_3_29/software/fun.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FUN_H
-#define __FUN_H
-
-void lcd_show(void);
-void key_scan(void);
-void led_scan(void);
-void data_show(void);
-uint16_t money(uint8_t *a,uint8_t *b,uint8_t flag);
-#endif
