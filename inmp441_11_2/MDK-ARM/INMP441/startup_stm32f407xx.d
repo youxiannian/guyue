468fb3a5e60b1f7f6e2d4d3b06b2a7f2bb28846b
youxiannian <2091478243@qq.com>
2025-11-30 15:14:53 +0800

    delate inmp441

--- inmp441_11_2/MDK-ARM/INMP441/startup_stm32f407xx.d
+++
@@ -1,1 +0,0 @@
-inmp441\startup_stm32f407xx.o: startup_stm32f407xx.s
--- inmp441_11_2/MDK-ARM/INMP441/testjitter.d
+++
@@ -1,6 +0,0 @@
-inmp441\testjitter.o: ..\speexdsp-1.2rc3\libspeexdsp\testjitter.c
-inmp441\testjitter.o: ..\speexdsp-1.2rc3\include\config.h
-inmp441\testjitter.o: ..\speexdsp-1.2rc3\include\speex/speex_jitter.h
-inmp441\testjitter.o: ..\speexdsp-1.2rc3\include\speex/speexdsp_types.h
-inmp441\testjitter.o: ..\speexdsp-1.2rc3\include\speex/speexdsp_config_types.h
-inmp441\testjitter.o: D:\software\keil_\ARM\ARMCC\Bin\..\include\stdio.h
